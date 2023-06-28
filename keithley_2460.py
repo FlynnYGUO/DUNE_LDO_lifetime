0e352a6bde2e5a200132709725ac1f16beb5701f
Eric <eraguzin@bnl.gov>
2023-06-28 18:31:03 -0400

    Small fixes after getting the assembled board and testing

--- keithley_2460.py
+++ keithley_2460.py
@@ -4,7 +4,7 @@ Created on Wed Jun 14 10:51:58 2023
 
 @author: Eraguzin
 """
-
+import time
 class Keithley2460:
     def __init__(self, rm, json_data):
         self.rm = rm
@@ -49,6 +49,9 @@ class Keithley2460:
         #Current measurements are auto
         self.keithley.write(f":SENSe1:CURRent:RANGe:AUTO {self.json_data['keithley2460_current_autorange']}")
         
+        #Set current limit for voltage measurements
+        self.keithley.write(f":SOUR:VOLT:ILIM {self.json_data['keithley2460_ilimit']}")
+        
         #Should we have a voltage limit or current limit? On Source or Measurement side?
         #Number of digits in ASCII response
         self.keithley.write(f":FORMat:ASCii:PRECision {self.json_data['keithley2460_ascii_digits']}")
@@ -63,10 +66,21 @@ class Keithley2460:
         self.keithley.write(f":DISPlay:USER2:TEXT:DATA '{self.json_data['keithley2460_front_panel2']}'")
         self.keithley.write(":DISPlay:SCReen SWIPE_USER")
         
+        #Get the slow reads out of the way
+        self.keithley.query(":MEASure:VOLTage?")
+        self.keithley.query(":MEASure:CURRent?", delay=5)
+        
     def measure(self):
         #Make measurements, results come in like '7.999993E+00\n'
         #Remove newline at end
-        voltage = self.keithley.query(":MEASure:VOLTage?").strip()
-        current = self.keithley.query(":MEASure:CURRent?").strip()
-        
+        try:
+            voltage = self.keithley.query(":MEASure:VOLTage?").strip()
+        except:
+            print("Keithley 2470 --> Error polling voltage")
+            voltage = 0
+        try:
+            current = self.keithley.query(":MEASure:CURRent?").strip()
+        except:
+            print("Keithley 2470 --> Error polling current")
+            current = 0
         return float(voltage),float(current)
